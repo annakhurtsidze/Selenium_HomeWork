0c0f1302397b891d0754494233048a9f6e5cb9a3
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-12-17 19:52:47 +0400

    test

---
+++ src/test/java/WebFormsTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebFormsTest {
+
+    @Test
+    public void webFormsTest(){
+        System.setProperty("Webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+
+        driver.findElement(By.xpath(".//select[@id='dropdowm-menu-1']")).click();
+        driver.findElement(By.xpath(".//option[@value='python']")).click();
+//        System.out.println(driver.findElement(By.xpath(".//option[@value='python']")).getText());
+        Assert.assertEquals(driver.findElement(By.xpath(".//option[@value='python']")).getText(), "Python");
+        driver.findElement(By.xpath(".//input[@value='option-1']")).click();
+        driver.findElement(By.xpath(".//input[@value='option-2']")).click();
+        driver.findElement(By.xpath(".//input[@value='option-4']")).click();
+        driver.findElement(By.xpath(".//input[@type='radio' and @value='yellow']")).click();
+        driver.findElement(By.xpath(".//select[@id='fruit-selects']")).click();
+//        boolean orangeOptionState = driver.findElement(By.xpath(".//option[@value='orange']")).isEnabled();
+        Assert.assertFalse(driver.findElement(By.xpath(".//option[@value='orange']")).isEnabled());
+//        System.out.println(orangeOptionState);
+
+        driver.close();
+    }
+}
